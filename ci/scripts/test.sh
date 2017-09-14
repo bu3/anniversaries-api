3eeecfad12e3bbb5a72a6ed757c6b2b1d768152b
Fabio Mangione <fabio.mangione@gmail.com>
2017-09-14 15:14:55 +0100

    Add integration tests

--- ci/scripts/test.sh
+++ ci/scripts/test.sh
@@ -6,9 +6,4 @@ pwd
 ls -al
 
 cd anniversaries-api
-./gradlew test build
-
-echo '>>>>>>>>>>>>> Copying jar file'
-cp  -R ./build ../artifact/
-echo '>>>>>>>>>>>>> Copying CF Manifest'
-cp manifest.yml ../artifact
+./gradlew test
